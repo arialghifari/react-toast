6622197bae4683bcce60ce916caeee229ea66d40
arialghifari <arialghifari99@gmail.com>
2024-04-19 09:04:25 +0800

    Exercise 1: wiring up form controls

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,11 @@ import styles from './ToastPlayground.module.css'
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error']
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('')
+  const [selectedVariant, setSelectedVariant] = React.useState(
+    VARIANT_OPTIONS[0]
+  )
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,24 +29,31 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+            {VARIANT_OPTIONS.map((variant) => (
+              <label key={variant} htmlFor={variant}>
+                <input
+                  id={variant}
+                  type="radio"
+                  name="variant"
+                  value={variant}
+                  defaultChecked={variant === selectedVariant}
+                  onChange={() => setSelectedVariant(variant)}
+                />
+                {variant}
+              </label>
+            ))}
           </div>
         </div>
 
